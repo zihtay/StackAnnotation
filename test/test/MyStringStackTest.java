a753322d4c276003cbf67f850cc6dfbff42ff084
Tay Zi Heng <hengalicious@hotmail.com>
2020-09-15 13:18:05 +0800

    fail the test case

--- test/test/MyStringStackTest.java
+++ test/test/MyStringStackTest.java
@@ -33,8 +33,8 @@ public class MyStringStackTest extends TestCase {
 	public void testPush() {
 		String o1 = null;  
 		MyStringStack stack = new MyStringStack();
-		assertTrue (stack.isEmpty());
 		stack.push (s1);
+		assertTrue (stack.isEmpty());
         stack.push (s2);
         assertFalse (stack.isEmpty());
 	}
